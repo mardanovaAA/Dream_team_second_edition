a01ce4c665ef44589c157c5bce3d12ac02fdb7ca
Kharichkin <kharichkin.is@phystech.edu>
2021-12-10 13:23:12 +0300

    Подключение сторонней библиотеки SFML через CMake

---
+++ cmake-build-debug/CMakeFiles/life-like_cellular_automaton.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "D:/Me/MIPT/life-like_cellular_automaton/main.cpp" "D:/Me/MIPT/life-like_cellular_automaton/cmake-build-debug/CMakeFiles/life-like_cellular_automaton.dir/main.cpp.obj"
+  "D:/Me/MIPT/life-like_cellular_automaton/src/App.cpp" "D:/Me/MIPT/life-like_cellular_automaton/cmake-build-debug/CMakeFiles/life-like_cellular_automaton.dir/src/App.cpp.obj"
+  "D:/Me/MIPT/life-like_cellular_automaton/src/Automaton.cpp" "D:/Me/MIPT/life-like_cellular_automaton/cmake-build-debug/CMakeFiles/life-like_cellular_automaton.dir/src/Automaton.cpp.obj"
+  "D:/Me/MIPT/life-like_cellular_automaton/src/AutomatonView.cpp" "D:/Me/MIPT/life-like_cellular_automaton/cmake-build-debug/CMakeFiles/life-like_cellular_automaton.dir/src/AutomatonView.cpp.obj"
+  "D:/Me/MIPT/life-like_cellular_automaton/src/Cell.cpp" "D:/Me/MIPT/life-like_cellular_automaton/cmake-build-debug/CMakeFiles/life-like_cellular_automaton.dir/src/Cell.cpp.obj"
+  "D:/Me/MIPT/life-like_cellular_automaton/src/Rules.cpp" "D:/Me/MIPT/life-like_cellular_automaton/cmake-build-debug/CMakeFiles/life-like_cellular_automaton.dir/src/Rules.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../usables"
+  "../headers"
+  "/usr/include"
+  "D:/Me/MIPT/SFML-2.5.1/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/life-like_cellular_automaton.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/life-like_cellular_automaton.dir/main.cpp.obj"
+  "CMakeFiles/life-like_cellular_automaton.dir/src/App.cpp.obj"
+  "CMakeFiles/life-like_cellular_automaton.dir/src/Automaton.cpp.obj"
+  "CMakeFiles/life-like_cellular_automaton.dir/src/AutomatonView.cpp.obj"
+  "CMakeFiles/life-like_cellular_automaton.dir/src/Cell.cpp.obj"
+  "CMakeFiles/life-like_cellular_automaton.dir/src/Rules.cpp.obj"
+  "liblife-like_cellular_automaton.dll.a"
+  "life-like_cellular_automaton.exe"
+  "life-like_cellular_automaton.exe.manifest"
+  "life-like_cellular_automaton.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/life-like_cellular_automaton.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
